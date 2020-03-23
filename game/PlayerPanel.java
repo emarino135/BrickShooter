c00f2711fa1ba24dd425177503d4014427c49547
Edwin <60202421+emarino135@users.noreply.github.com>
2020-03-22 20:50:08 -0400

    Fixed packages
    Added packages "package game;" and added "import game.Ball;" to RunningGame.java

---
+++ game/PlayerPanel.java
@@ -0,0 +1,57 @@
+package game;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class PlayerPanel extends RectangleGenerator{
+   private int posX;
+   private int posY;
+   private int width;
+   private int height;
+   
+   public PlayerPanel(int posX, int posY, int width, int height){
+      super (posX, posY, width, height);
+      setLocation();
+      setDimension();
+   }
+   
+   public void setLocation(){
+      this.posX = 460;
+      this.posY = 900;
+   }
+   
+   public void setDimension(){
+      this.width = 150;
+      this.height= 50;
+   }
+   
+   public int getXPos(){
+      return this.posX;
+   }
+   
+   public int getYPos(){
+      return this.posY;
+   }
+   
+   public int getWidth(){
+      return this.width;
+   }
+   
+   public int getHeight(){
+      return this.height;
+   }
+   
+   public Rectangle getRect(){
+      return new Rectangle(this.posX, this.posY, this.width, this.height);
+   }
+   
+   public void moveRight(){
+      this.posX+=20;
+   }
+   
+   public void moveLeft(){
+      this.posX-=20;
+   }
+}
+
---
+++ game/README.md
@@ -0,0 +1,5 @@
+# BrickShooter game
+I created this game using Swing and AWT GUI libraries in Java. The game is simple and easy to play. However, it is near impossible
+  to win this game because the bricks, which are the bad guys, keep getting generated and moving vertically toward the player's panel
+  with the fast pace. The graphic is not that smooth and i believe it needs to be polished because I mainly used geometric shapes to 
+  for all objects in the game. 
